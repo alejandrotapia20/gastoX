a98839be96e080cdb05ebde71c98e84469b819e8
alejandrotapia20 <alejandrotapia20@hotmail.com>
2023-08-03 12:59:01 -0500

    Subiendo archivo

--- for.py
+++
@@ -1,6 +0,0 @@
-tabla = int(input("Ingresar la tabla que desea realizar: "))
-hasta = int(input("Ingresar hasta que numero desea multiplicar: "))
-
-for i in range(1,hasta+1,1):
-    c = tabla * i
-    print(tabla, "x", i, "=", c)
--- tabla_multiplicar.py
+++
@@ -1,7 +0,0 @@
-i=0
-t=int(input("Ingrese el numero de la tabla que desea realizar:"))
-
-while i<=10:
-    result = t * i
-    print(t, "x", i, "=", result)
-    i = i + 1
